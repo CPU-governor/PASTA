1d2caa5664268be9739ba1826c0aeaf72884c4bb
Okuchukwu Nzeogwu <cpu.governor@gmail.com>
2024-04-20 20:58:50 +0100

    Update main.pas

--- src/main.pas
+++ src/main.pas
@@ -1,3 +1,19 @@
+// CPU Governor [cpu.governor@gmail.com]
+// April 20, 2024
+
+
+// LIBRARIES
+uses
+    Graph;
+
+
+// GlOBAL VARIABLES
+
+
+
 begin
-// nothing to see yet
+WriteLn("====================PASTA====================");
+WriteLn("1. Import values from a CSV file");
+WriteLn("2. Manually Iput values");
+WriteLn("3. Exit");
 end
